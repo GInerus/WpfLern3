b497ad785033ae6932a7bbe0f1f97a1598b255fe
Герман Исаев <germanisaev422@gmail.com>
2025-11-18 00:06:55 +0300

    Создан пустой обработчик событий для кнопки Регистрации

--- WpfLern3/MainWindow.xaml.cs
+++ WpfLern3/MainWindow.xaml.cs
@@ -20,5 +20,10 @@ namespace WpfLern3
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
