6a75d34aa399b3b5567f09a2af514e9cea1f84ab
Герман Исаев <germanisaev422@gmail.com>
2025-11-18 00:49:20 +0300

    Корекция и проверка значений в полях ввода

--- WpfLern3/MainWindow.xaml.cs
+++ WpfLern3/MainWindow.xaml.cs
@@ -21,9 +21,46 @@ namespace WpfLern3
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void ButtonRegistration_Click(object sender, RoutedEventArgs e)
         {
+            string enteredLogin = textBoxLogin.Text.Trim();
+            string enteredPasswordFirst = passwordBoxFirst.Password.Trim();
+            string enteredPasswordSecond = passwordBoxSecond.Password.Trim();
+            string enteredEmail = textBoxEmail.Text.Trim().ToLower();
 
+            if (enteredLogin.Length < 4)
+            {
+                textBoxLogin.ToolTip = "Логин не может быть меньше 4 символов";
+                textBoxLogin.Background = Brushes.DarkRed;
+            }
+            else if (enteredPasswordFirst.Length < 8)
+            {
+                passwordBoxFirst.ToolTip = "Пароль не может быть меньше 48 символов";
+                passwordBoxFirst.Background = Brushes.DarkRed;
+            }
+            else if (enteredPasswordFirst != enteredPasswordSecond)
+            {
+                passwordBoxSecond.ToolTip = "Пороли не савподают";
+                passwordBoxSecond.Background = Brushes.DarkRed;
+            }
+            else if (enteredEmail.Length > 6 || enteredEmail.Contains("@") || enteredEmail.Contains("."))
+            {
+                textBoxEmail.ToolTip = "Поле заполнено неверно";
+                textBoxEmail.Background = Brushes.DarkRed;
+            }
+            else
+            {
+                textBoxLogin.ToolTip = "";
+                textBoxLogin.Background = Brushes.Transparent;
+                passwordBoxFirst.ToolTip = "";
+                passwordBoxFirst.Background = Brushes.Transparent;
+                passwordBoxSecond.ToolTip = "";
+                passwordBoxSecond.Background = Brushes.Transparent;
+                textBoxEmail.ToolTip = "";
+                textBoxEmail.Background = Brushes.Transparent;
+
+                MessageBox.Show("Заебись получается!!!");
+            }
         }
     }
 }
